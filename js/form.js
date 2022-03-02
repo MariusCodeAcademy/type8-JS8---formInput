c8d63f76d58ae8773d099e73217beb87cadb2f74
Marius <marius@example.com>
2022-03-02 11:04:46 +0200

    slider

--- js/form.js
+++ js/form.js
@@ -6,10 +6,14 @@ const formEl = document.getElementById('userForm');
 const usernameEL = document.getElementById('username');
 const emailEL = document.getElementById('email');
 const townSelectEl = document.getElementById('town');
+const rangeEl = document.getElementById('points');
+
 // result EL
 const out1El = document.getElementById('out1');
 const out2El = document.getElementById('out2');
 const out3El = document.getElementById('out3');
+const sliderValueEl = document.querySelector('.value');
+
 // debugger;
 // visada tuscia
 // const usernameValue = usernameEL.value;
@@ -21,20 +25,52 @@ formEl.addEventListener('submit', (event) => {
   console.log('in controll');
   // clear errors
   usernameEL.nextElementSibling.textContent = '';
-  // reiksmiu paemimas
-  const usernameValue = usernameEL.value;
+  // reiksmiu paemimas // trim nukerpa tuscius tarpus prieky ir gale
+  const usernameValue = usernameEL.value.trim();
   const emailValue = emailEL.value;
   const townValue = townSelectEl.value;
+  const rangeValue = rangeEl.value;
+  console.log('rangeValue ===', rangeValue);
 
   // validation
-  if (usernameValue.length <= 3) {
-    // nusitaikom i po input einanti el
-    usernameEL.nextElementSibling.textContent = 'Per trumpas username';
+  if (!isValid(usernameValue)) {
     return;
   }
 
   // reiksmiu perkelimas i rezultatus
+  valuesToResults(usernameValue, emailValue, townValue);
+});
+
+function valuesToResults(usernameValue, emailValue, townValue) {
+  // reiksmiu perkelimas i rezultatus
   out1El.textContent = usernameValue;
   out2El.textContent = emailValue;
   out3El.textContent = townValue;
-});
+}
+
+function isValid(value) {
+  if (value.length <= 3) {
+    usernameEL.nextElementSibling.textContent = 'Per trumpas username';
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// range handler
+rangeEl.addEventListener('input', sliderValueHandle);
+
+function sliderValueHandle(event) {
+  // const sliderValueEl = document.querySelector('.value');
+  // event.target - yra rangeEl
+  // event.target.value - dabartine reiksme
+  const reiksme = event.target.value;
+  // ikelti reiksme is sliderio i  <strong class="value"></strong>
+  sliderValueEl.textContent = reiksme;
+  // console.log(event);
+}
+
+function startValueSlider() {
+  sliderValueEl.textContent = rangeEl.value;
+}
+startValueSlider();
